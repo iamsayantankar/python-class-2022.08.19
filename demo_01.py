b9fd416006602753af4420ca8750588dfc281e3f
sayan <sayantankar01@gmail.com>
2022-08-19 20:27:57 +0530

    Initial commit

---
+++ demo_01.py
@@ -0,0 +1,4 @@
+a=5
+b=8
+c=a+b
+print ("The value will be" , c)
---
+++ demo_02.py
@@ -0,0 +1,6 @@
+a=5
+b=6
+print("Sum: ",a+b,end =" foe add two string in same line ") #use end="" in print statement for add a single line from two print statement
+print("sub: ",a-b)
+print("multi: ",a*b)
+print("div: ",a/b)
